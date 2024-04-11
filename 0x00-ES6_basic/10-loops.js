2917c3ecfda6fa911c09a87328a1853c51f68884
mugishab2020 <mugishab2020@gmail.com>
2024-04-11 12:27:36 +0200

    Now we got this

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -4,6 +4,6 @@ export default function appendToEachArrayValue(array, appendString) {
   for (const value of array) {
     newArray.push(appendString + value);
   }
-
-  return array;
+  
+  return newArray;
 }
