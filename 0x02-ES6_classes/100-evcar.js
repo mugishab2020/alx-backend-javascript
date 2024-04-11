0afa638c83e751c673e801995bef75705ac94ca6
mugishab2020 <mugishab2020@gmail.com>
2024-04-12 00:18:29 +0200

    Now we got this

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,11 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super();
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+    this._range = range;
+  }
+}
