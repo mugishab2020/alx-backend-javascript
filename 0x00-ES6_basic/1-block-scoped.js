a5301d2c16c88fc009590da07df75ef90c88fef8
mugishab2020 <mugishab2020@gmail.com>
2024-04-11 10:58:39 +0200

    we git this

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
     const task2 = true;
   
     if (trueOrFalse) {
-      var task = true;
-      var task2 = false;
+      task;
+      task2;
     }
   
     return [task, task2];
