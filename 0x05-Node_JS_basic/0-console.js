6d830d37002783e909bbfb5c72588b0acda0a980
mugishab2020 <mugishab2020@gmail.com>
2024-06-04 13:54:02 +0200

    Eslint solved

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /*
 this is just a function that will display a given message
 */
-function displayMessage(message){
-    console.log(message);
+function displayMessage(message) {
+  console.log(message);
 }
 module.exports = displayMessage;
--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,16 @@
 /*
-program to read the name from the user and display it 
-and when closing we print the message*/
+program to read the name from the user and display it
+and when closing we print the message */
 const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-  console.log("Welcome to Holberton School, what is your name?\n");
-   readline.question('Enter your name: ', (name) => {
-    console.log(`Your name is: ${name}\n`);
-    readline.close();
-  });
-  process.on('exit', () => {
-    console.log("This important software is now closing\n");
-  });
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Welcome to Holberton School, what is your name?\n');
+readline.question('Enter your name: ', (name) => {
+  console.log(`Your name is: ${name}\n`);
+  readline.close();
+});
+process.on('exit', () => {
+  console.log('This important software is now closing\n');
+});
